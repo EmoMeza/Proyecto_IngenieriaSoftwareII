3dcd0b7220eba1e8c8c29cb24db8269cb9a1bd1a
EmoMeza <emeza2019@inf.udec.cl>
2023-04-24 23:54:59 -0400

    Fixes
    Arreglé cosas de los endpoints, ademas de agregar un archivo json con todos los endpoints apis para facilitar trabajo en frontend

--- controllers/report_controller.py
+++ controllers/report_controller.py
@@ -10,19 +10,21 @@ report_controller = Blueprint('report_controller', __name__)
 @report_controller.route('/reports/comments', methods=['POST'])
 def add_comment():
     data=request.json
+    id_report = request.args.get('id_report')
+    #check if the id_report is in the database
+    if database.reporte.query.filter_by(id=id_report).first() == None:
+        return jsonify({'message': 'The id_report is not in the database'}), 400
     if data['text'] == None:
         return jsonify({'message': 'Comment not added. Text is needed'}), 400
-    if data['id_report'] == None:
-        return jsonify({'message': 'Comment not added. Id_report is needed'}), 400
-    add_comentario(data['text'], data['id_report'])
+    add_comentario(data['text'], id_report)
     db.session.commit()
     return jsonify({'message': 'Comment added successfully.'}), 201
 
 #ENDPOINT API to recieve a like and add it to the database
 @report_controller.route('/reports/like', methods=['POST'])
 def add_like():
-    data=request.json
-    id_report = data['id_report']
+
+    id_report = request.args.get('id_report')
     report = database.reporte.query.get_or_404(id_report)
     report.add_likes(1)
     db.session.commit()
@@ -32,30 +34,34 @@ def add_like():
 @report_controller.route('/reports/add', methods=['POST'])
 def add_report():
     data=request.json
+    id_product = request.args.get('id_product')
+    if database.producto.query.filter_by(id=id_product).first() == None:
+        return jsonify({'message': 'The id_product is not in the database'}), 400
     if data['title'] == None:
         return jsonify({'message': 'Report not added. Title is needed'}), 400
     if data['description'] == None:
         return jsonify({'message': 'Report not added. Description is needed'}), 400
-    if data['id_product'] == None:
-        return jsonify({'message': 'Report not added. Id_product is needed'}), 400
-    add_reporte(data['title'], data['description'], data['id_product'])
+    add_reporte(data['title'], data['description'], id_product)
     db.session.commit()
     return jsonify({'message': 'Report added successfully.'}), 201
 
 
 @report_controller.route('/reports/get', methods=['GET'])
 def get_report():
-    data=request.json
-    quantity = data['quantity']
-    id_product = data['id_product']
+    quantity = request.args.get('quantity')
+    id_product = request.args.get('id_product')
     #check if the id_product is in the database
     if database.producto.query.filter_by(id=id_product).first() == None:
         return jsonify({'message': 'The id_product is not in the database'}), 400
-    reports = database.reporte.query.filter_by(id_producto=id_product).all()
-    if int(quantity) > len(reports):
-        return jsonify({'message': 'The number of reports asked is bigger than the number of reports in the database. The number of reports in the database is ' + str(len(reports)) + ' and the number asked is ' + str(quantity)}), 400
-    reports = reports[-int(quantity):]
-    reports_json = []
+    if quantity == None:
+        return jsonify({'message': 'Quantity not specified'}), 400
+    #check if the quantity is bigger than the number of reports
+    if int(quantity) > database.reporte.query.count():
+        #return the number of quantity asked and the number of reports with the product id
+        return jsonify({'message': 'The quantity is bigger than the number of reports. Quantity asked is: '+ quantity+' The number of reports with the product id is: ' + str(database.reporte.query.filter_by(id_producto=id_product).count())}), 400
+
+    reports = database.reporte.query.filter_by(id_producto=id_product).order_by(database.reporte.likes.desc()).limit(quantity).all()
+    reports_json = []   
     for report in reports:
         report_json = {}
         report_json['id'] = report.id
