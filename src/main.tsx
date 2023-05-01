d154555859a03b0cf679a058622f54fd9fd44369
tiagowhuber <131737310+tiagowhuber@users.noreply.github.com>
2023-05-01 11:34:37 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,29 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import App from "./routes/App";
-import IngresarBug from "./routes/IngresarBug";
+import IngresarBug from "./routes/IngresarBug"
+import VerReporte from "./routes/VerReporte"
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-    errorElement: "Error",
+    path:"/",
+    element: <App/>, 
+    errorElement:"Error",
   },
   {
     path: "/IngresarBug",
-    element: <IngresarBug />,
+    element: <IngresarBug/>,
   },
+  {
+    path: "/VerReporte",
+    element: <VerReporte/>,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
+  <React.StrictMode> 
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
