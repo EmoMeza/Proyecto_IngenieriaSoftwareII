bc9a7a75a60b9bc2780202150fd43a8c0fe66061
MIchael Ignacio Villanueva Torres <mvillanueva2019@inf.udec.cl>
2023-06-23 18:12:48 -0400

    LIKE con un imagen xd y no se puede dar like más de una vez, USUARIO Hardcodeado :c es el numero 2

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Bug from "./Bug";
 
-const LikeButton = (props: { id_bug: number }) => {
+
+const LikeButton = (props: { id_bug: number; id_user:number }) => {
   const addLike = async () => {
     try {
-      const response = await fetch(`http://127.0.0.1:5000/reports/like?id_report=${props.id_bug}`, {
+      const response = await fetch(`http://127.0.0.1:5000/reports/like?id_report=${props.id_bug}&id_user=${props.id_user}`, {
         method: 'POST',
       });
       const data = await response.json();
@@ -15,8 +16,7 @@ const LikeButton = (props: { id_bug: number }) => {
   };
 
   return (
-    //<button className="btn btn-primary btn-edit like-Button"> onClick={() => handleLike(bug.id)} +</button>
-    <button className ="btn btn-primary btn-edit like-Button" onClick={addLike} type='submit'>+</button>
+    <button className ="btn btn-primary btn-edit like-Button" onClick={addLike} type='submit'><img width={25} height={25} src="https://depor.com/resizer/OkW8HA6832U2SsV1b6RooS-3Oug=/1200x1200/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/TASGMCIJHFAKLMEAXIMVGEYXPM.jpg"/></button>
   );
 };
 
